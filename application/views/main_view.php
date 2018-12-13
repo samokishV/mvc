4f9698dc53a505b2cc2372a695bdf28cecd9b67b
Viktoria Samokish <samokish.viktoria@gmail.com>
2018-12-13 20:44:01 +0200

    Add home page

---
+++ application/views/main_view.php
@@ -0,0 +1,9 @@
+<div class="cover">
+  <div class="inner-cover">
+    <h1 class="cover-heading">Settled nature in your home</h1>
+    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p class="lead">
+      <a href="product_list.php" class="btn btn-lg btn-primary">Go to shopping</a>
+    </p>
+  </div>
+</div>
