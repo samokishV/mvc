2f08973c26444a14de38c7abba0fdfefcc578a8e
Viktoria Samokish <samokish.viktoria@gmail.com>
2018-12-14 13:18:48 +0200

    Add product page

--- application/views/main_view.php
+++ application/views/main_view.php
@@ -1,7 +1,8 @@
 <div class="cover">
   <div class="inner-cover">
     <h1 class="cover-heading">Settled nature in your home</h1>
-    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.</p>
+    <p class="lead">Cover is a one-page template for building simple and beautiful home pages. Download, edit the text, and add your own fullscreen background photo to make it your own.
+		</p>
     <p class="lead">
       <a href="/stock/" class="btn btn-lg btn-primary">Go to shopping</a>
     </p>
