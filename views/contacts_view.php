61909a4cdd329ba0c271aed7fe874295d128c4cd
Viktoria Samokish <samokish.viktoria@gmail.com>
2019-01-16 18:56:54 +0200

    Added validation to contact form

--- views/contacts_view.php
+++ views/contacts_view.php
@@ -6,24 +6,36 @@
                 <label for="name" class="col-2 col-form-label">Name</label>
                 <div class="col-10">
                     <input class="form-control" type="text" value="" id="name" name="name" required>
+                    <div class="invalid-feedback">
+                        Please enter your name.
+                    </div>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="email" class="col-2 col-form-label">Email</label>
                 <div class="col-10">
                     <input class="form-control" type="email" value="" id="email" name="email" required>
+                    <div class="invalid-feedback">
+                        Please provide a valid email.
+                    </div>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="subject" class="col-2 col-form-label">Subject</label>
                 <div class="col-10">
                     <input class="form-control" type="text" value="" id="subject" name="subject" required>
+                    <div class="invalid-feedback">
+                        Please enter message subject.
+                    </div>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="message" class="col-2 col-form-label">Message</label>
                 <div class="col-10">
                     <textarea class="form-control" id="message" rows="5" name="message" required></textarea>
+                    <div class="invalid-feedback">
+                        Please enter your message.
+                    </div>
                 </div>
             </div>
             <input type="submit" class="btn btn-success btn-lg w-100" value="Send">
