a66fca8a8750a2d6320456d17ec3df017883a885
Viktoria Samokish <samokish.viktoria@gmail.com>
2019-01-16 18:55:17 +0200

    Added validation to contact form

--- views/contacts_view.php
+++ views/contacts_view.php
@@ -1,29 +1,29 @@
 <div class="cover">
     <div class="inner-cover">
-        <form method="post" action="" class="contacts-form">
+        <form method="post" action="" class="contacts-form needs-validation" novalidate>
             <h5 class="text-uppercase bold">Contact us</h5>
             <div class="form-group row">
                 <label for="name" class="col-2 col-form-label">Name</label>
                 <div class="col-10">
-                    <input class="form-control" type="text" value="" id="name" name="name">
+                    <input class="form-control" type="text" value="" id="name" name="name" required>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="email" class="col-2 col-form-label">Email</label>
                 <div class="col-10">
-                    <input class="form-control" type="email" value="" id="email" name="email">
+                    <input class="form-control" type="email" value="" id="email" name="email" required>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="subject" class="col-2 col-form-label">Subject</label>
                 <div class="col-10">
-                    <input class="form-control" type="text" value="" id="subject" name="subject">
+                    <input class="form-control" type="text" value="" id="subject" name="subject" required>
                 </div>
             </div>
             <div class="form-group row">
                 <label for="message" class="col-2 col-form-label">Message</label>
                 <div class="col-10">
-                    <textarea class="form-control" id="message" rows="5" name="message"></textarea>
+                    <textarea class="form-control" id="message" rows="5" name="message" required></textarea>
                 </div>
             </div>
             <input type="submit" class="btn btn-success btn-lg w-100" value="Send">
